d5ec153f97b40af6677f119c5937418664b409f1
Karina Guerra <kpguerra96@gmail.com>
2020-09-20 04:39:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,27 @@
 # Write your methods here
+
+def loop_message_five_times (message)
+  counter = 0
+  for counter in 1..5
+    puts message
+  end 
+end 
+
+def loop_message_n_times (message, integer)
+  counter = 0
+ until (counter == integer) do
+    puts message
+    counter += 1
+  end 
+end 
+
+def output_array (array)
+  counter = 0
+  until counter == array.length do
+    puts array[counter]
+    counter += 1
+  end 
+end 
+
+def return_string_array (array)
+  
